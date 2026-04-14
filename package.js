31b3d3679b24e0e00363c3ef6afb29707b66064f
Abhishek <abhishekdandgi2003@gmail.com>
2026-04-14 11:24:22 +0530

    Create package.js

---
+++ package.js
@@ -0,0 +1,18 @@
+{
+  "name": "vtudeveloper-signaling-server",
+  "version": "1.0.0",
+  "description": "WebRTC Signaling Server for VTUDeveloper Skills Online Class",
+  "main": "server.js",
+  "scripts": {
+    "start": "node server.js",
+    "dev": "nodemon server.js"
+  },
+  "dependencies": {
+    "cors": "^2.8.5",
+    "express": "^4.18.2",
+    "socket.io": "^4.7.2"
+  },
+  "devDependencies": {
+    "nodemon": "^3.0.1"
+  }
+}
